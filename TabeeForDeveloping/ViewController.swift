ce8659d59b2fddf112b9a642bd4d8b7414d91247
Sergey Solomonov <ssolomonov@broniboy.ru>
2022-06-02 16:50:58 +0300

    Initial Commit

---
+++ TabeeForDeveloping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabeeForDeveloping
+//
+//  Created by Serg on 02.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
